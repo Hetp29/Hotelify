e9771c9bf744bbbb6cafb04f20d05297deffb694
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-08-07 10:18:03 -0400

    Setup express API endpoint

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import "dotenv/config";
+
+const app = express(); //create express app
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+app.get("/api/test", async (req, res)=> {
+    res.json({ message: "hello from api endpoint"});
+});
+app.listen(7050, ()=>{
+    console.log("server is running on port 7050");
+})
